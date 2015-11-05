d142db234e4a30574cc1203af2f1ff99e70e0ece
xingmin860818 <763513751@qq.com>
2015-11-05 13:01:02 +0800

    Update get_img_url_test.py

--- get_img_url_test.py
+++ get_img_url_test.py
@@ -1,5 +1,6 @@
 #测试用例
 import unittest
+import re
 from re_query_url import *
 
 class Test_get_url(unittest.TestCase):
@@ -9,6 +10,14 @@ class Test_get_url(unittest.TestCase):
 	def test_init(self):
 		self.assertTrue(isinstance(self.p,list))
 		self.assertTrue(isinstance(self.q,list))
+        def test_if_img(self):
+                d = ['jpg','png','gif','jpeg']
+                for i in self.p:
+                        res = re.match(r'(.+)\.(.*)',i).group(2)
+                        self.assertTrue(res in d)
+                for j in self.q:
+                        res = re.match(r'(.+)\.(.*)',j).group(2)
+                        self.assertTrue(res in d)
 	def test_attr(self):
 		self.p.append('test')
 		self.assertEqual(self.p[-1],'test')
